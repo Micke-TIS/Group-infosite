1e3551a243916e4f7ace0439292901d01437df11
Shadmehr <tismma19shaehs@innovationsgymnasiet.se>
2021-03-24 11:13:28 +0100

    menu to dynamic

--- index.php
+++ index.php
@@ -27,25 +27,9 @@ include_once("includes/header.php");
                         </a>
                         <!-- ***** Logo End ***** -->
                         <!-- ***** Menu Start ***** -->
-                        <ul class="nav">
-                            <li class="scroll-to-section"><a href="#welcome" class="menu-item">Home</a></li>
-                            <li class="scroll-to-section"><a href="#about" class="menu-item">About</a></li>
-                            <li class="scroll-to-section"><a href="#testimonials" class="menu-item">Testimonials</a>
-                            </li>
-                            <li class="submenu">
-                                <a href="javascript:;">Drop Down</a>
-                                <ul>
-                                    <li><a href="" class="menu-item">About Us</a></li>
-                                    <li><a href="" class="menu-item">Features</a></li>
-                                    <li><a href="" class="menu-item">FAQ's</a></li>
-                                    <li><a href="" class="menu-item">Blog</a></li>
-                                </ul>
-                            </li>
-                            <li class="scroll-to-section"><a href="#contact-us" class="menu-item">Contact Us</a></li>
-                        </ul>
-                        <a class='menu-trigger'>
-                            <span>Menu</span>
-                        </a>
+                        <?php
+                        include_once("includes/menu.php");
+                        ?>
                         <!-- ***** Menu End ***** -->
                     </nav>
                 </div>
@@ -53,8 +37,9 @@ include_once("includes/header.php");
         </div>
     </header>
     <!-- ***** Header Area End ***** -->
-
-
+    <?php
+    include_once("includes/header.php");
+    ?>
     <!-- ***** Welcome Area Start ***** -->
     <div class="welcome-area" id="welcome">
 
