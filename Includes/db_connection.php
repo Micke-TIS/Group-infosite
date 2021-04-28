4b1e8db9c35c11fd892b1c1649b95c24bcdd2746
Mateusz <tismma18matzan@innovationsgymnasiet.se>
2021-04-28 11:02:28 +0200

    .

--- Includes/db_connection.php
+++ Includes/db_connection.php
@@ -1,18 +1,19 @@
 <?php
-function OpenCon()
- {
- $dbhost = "localhost";
- $dbuser = "root";
- $dbpass = "1234";
- $db = "example";
- $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+//Database connection
+$servername='localhost';
+$username='root';
+$password='';
+$dbname = "example";
+$conn=mysqli_connect($servername,$username,$password,"$dbname");
+if(!$conn){
+die('Could not Connect My Sql:' .mysql_error());
+}
 
- return $conn;
- }
-
-function CloseCon($conn)
- {
- $conn -> close();
- }
-
+//Select specific columns from DB
+$query = mysqli_query($conn, "SELECT * FROM tbl_articles");
+while($row = mysqli_fetch_assoc($query))
+{
+$menu_about = $row['about'];
+$menu_features = $row['features'];
+}
 ?>
--- Includes/menu.php
+++ Includes/menu.php
@@ -7,7 +7,7 @@
     <li class="submenu">
         <a href="javascript:;">Drop Down</a>
         <ul>
-            <li><a href="" class="menu-item">About Us</a></li>
+            <li><a href="" class="menu-item"><?php echo $about; ?></a></li>
             <li><a href="" class="menu-item">Features</a></li>
             <li><a href="" class="menu-item">FAQ's</a></li>
             <li><a href="" class="menu-item">Blog</a></li>
