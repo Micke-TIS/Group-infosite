a5ddd4c7d4a3df9cde83de5216153a11b543d71f
Mateusz <tismma18matzan@innovationsgymnasiet.se>
2021-04-28 11:03:44 +0200

    ,

--- Includes/config.php
+++ Includes/config.php
@@ -1,23 +1,4 @@
 <?php
-function OpenCon()
- {
- $dbhost = "localhost";
- $dbuser = "root";
- $dbpass = "1234";
- $db = "example";
- $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
-
- return $conn;
- }
-
-function CloseCon($conn)
- {
- $conn -> close();
- }
-
-?>
-
-<?php
 include 'db_connection.php';
 $conn = OpenCon();
 echo "Connected Successfully";
