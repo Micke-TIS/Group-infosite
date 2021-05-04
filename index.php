bb3b3c83001a6970f5382c5daf7c46ca4ef526f1
Micke <mikael.johanssonj@gmail.om>
2021-05-04 16:22:38 +0200

    dsddd

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <?php
 include_once("includes/header.php");
-include_once("includes/db_connection.php");
+include_once("includes/config.php");
 ?>
-
-
-
-
-
   <?php
   include_once("includes/menu_area.php");
   ?>
