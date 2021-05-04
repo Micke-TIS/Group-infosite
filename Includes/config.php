dea75004362c3dd1af5c167a30ee8bef84f4e1b0
Micke <mikael.johanssonj@gmail.om>
2021-05-04 16:27:14 +0200

    ddd

--- Includes/config.php
+++ Includes/config.php
@@ -14,11 +14,11 @@ die('Could not Connect My Sql:' .mysql_error());
 $query = mysqli_query($conn, "SELECT * FROM tbl_articles");
 while($row = mysqli_fetch_assoc($query))
 {
-$menu_about = $row['about'];
-$menu_features = $row['features'];
-$menu_faq = $row['faq'];
-$menu_blog = $row['blog'];
-$menu_contact = $row['contact'];
+$menu_about = $row['menu_about'];
+$menu_features = $row['menu_features'];
+$menu_faq = $row['menu_faq'];
+$menu_blog = $row['menu_blog'];
+$menu_contact = $row['menu_contact'];
 }
 ?>
 
