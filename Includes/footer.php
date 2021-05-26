c0970c82dc7451247028a32c196c0b8476b5c03b
oliver <tismma19olisun@innovationsgymnasiet.se>
2021-05-26 11:35:11 +0200

    t

--- Includes/footer.php
+++ Includes/footer.php
@@ -22,11 +22,11 @@ Email Id:<br>
 </form>
                 <div class="col-lg-6 col-md-12 col-sm-12">
                     <div class="contact-form">
-                        <form id="contact" action="" method="post">
+                        <form id="myForm" method="post" action="process.php">
                             <div class="row">
                                 <div class="col-md-6 col-sm-12">
                                     <fieldset>
-                                        <input name="name" type="text" id="name" placeholder="Full Name" required=""
+                                        <input name="first_name" type="text" id="name" placeholder="Full Name" required=""
                                             style="background-color: rgba(250,250,250,0.3);">
                                     </fieldset>
                                 </div>
